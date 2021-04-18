b5d17e32c80939b8b57396cdbc8d2b282c80d531
robbedeboosere <deboosererobbe2001@gmail.com>
2021-04-18 18:33:27 +0200

    add moves to events

--- website/games/events.py
+++ website/games/events.py
@@ -23,3 +23,40 @@ def start_game(data):
     game = gameManager.current_games[room]
     game.start_game()
     emit('status', {"event": "startgame", "username": session.get("username")}, room=room)
+
+@socketio.on("throwcard")
+def throw_card(cards):
+    room = session.get("room")
+    game = gameManager.current_games[room]
+    player = game.get_player(session.get("username"))
+    game.throw_cards(player, cards)
+    emit('move', {"event": "throwcard", "username": session.get("username")}, room=room)
+
+@socketio.on("takecards")
+def take_cards():
+    room = session.get("room")
+    game = gameManager.current_games[room]
+    player = game.get_player(session.get("username"))
+    game.take_cards(player)
+    emit('move', {"event": "takecards", "username": session.get("username")}, room=room)
+
+@socketio.on("breakcards")
+def take_cards():
+    room = session.get("room")
+    game = gameManager.current_games[room]
+    game.break_cards()
+    emit('move', {"event": "breakcards", "username": session.get("username")}, room=room)
+
+@socketio.on("breakcard")
+def break_card(bottomcard,topcard):
+    room = session.get("room")
+    game = gameManager.current_games[room]
+    game.break_card(bottomcard,topcard)
+    emit('move', {"event": "breakcard", "username": session.get("username")}, room=room)
+
+@socketio.on("passcards")
+def pass_cards(cards):
+    room = session.get("room")
+    game = gameManager.current_games[room]
+    game.pass_on(cards)
+    emit('move', {"event": "passcards", "username": session.get("username")}, room=room)
