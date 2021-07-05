4c1f78e0011acb0ef39f7abc7d283cb56c196ea0
lennertcl <lennert.claerhout@hotmail.com>
2021-07-05 19:57:24 +0200

    add option to change password (closes #18)

--- website/users/routes.py
+++ website/users/routes.py
@@ -64,14 +64,14 @@ def account():
 
 @users.route("/reset_password", methods=['GET', 'POST'])
 def reset_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.home'))
     form = RequestResetForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         send_reset_email(user)
         flash('An email has been sent with instructions to reset your password.', 'info')
         return redirect(url_for('users.login'))
+    if current_user.is_authenticated:
+        form.email.data = current_user.email
     return render_template('user/reset_request.html', title='Reset Password', form=form)
 
 
