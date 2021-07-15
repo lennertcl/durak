3ba8fc9fc3a925a3ddd2bd93a241a0782160b997
lennertcl <lennert.claerhout@hotmail.com>
2021-07-15 19:11:30 +0200

    add start game test (#9)

--- test/durak_test.py
+++ test/durak_test.py
@@ -7,25 +7,35 @@ from website.durak_game.durak import DurakGame
 from .fixtures import game
 
 
-# GAME STATUS
+# START GAME
 
 
-def test_lobby(game):
-    """ Test correct game initialization """
-    assert game.get_lobby_count() == 4
-    assert game.get_player_count() == 0
-
+def test_start_game(game):
+    """ Test correct starting of game """
     game.start_game()
+
+    assert game.cards_per_player == 6
 
     assert game.get_lobby_count() == 0
     assert game.get_player_count() == 4
 
+    assert game.deck is not None
+
     for player in game.players:
         assert player.get_card_count() == game.cards_per_player
 
+    assert game.trump is not None
+    assert game.trump in Card.SUITS
+    assert game.trump_card is not None
+    assert game.trump_card.get_suit() == game.trump
 
-# START GAME
-# TODO
+    assert game.current_player is not None
+
+    assert not game.throwing_started
+    assert not game.next_allows_break
+    assert not game.prev_allows_break
+
+    assert game.is_in_progress
 
 
 # FINISH ROUND
